5c1f087e9000a68a87008df4483a6e536c75edc4
muflihanto <muflihanto@gmail.com>
2022-09-27 03:43:11 +0700

    Editing chirps

--- resources/js/Components/Chirp.jsx
+++ resources/js/Components/Chirp.jsx
@@ -1,15 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import Dropdown from "@/Components/Dropdown";
+import InputError from "@/Components/InputError";
+import PrimaryButton from "@/Components/PrimaryButton";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
+import { useForm, usePage } from "@inertiajs/inertia-react";
 
 dayjs.extend(relativeTime);
 
 export default function Chirp({ chirp }) {
+  const { auth } = usePage().props;
+
+  const [editing, setEditing] = useState(false);
+
+  const { data, setData, patch, processing, reset, errors } = useForm({
+    message: chirp.message,
+  });
+
+  const submit = (e) => {
+    e.preventDefault();
+    patch(route("chirps.update", chirp.id), { onSucess: () => setEditing(false) });
+  };
+
   return (
-    <div className="p-6 flex space-x-2">
+    <div className="flex p-6 space-x-2">
       <svg
         xmlns="http://www.w3.org/2000/svg"
-        className="h-6 w-6 text-gray-600 -scale-x-100"
+        className="w-6 h-6 text-gray-600 -scale-x-100"
         fill="none"
         viewBox="0 0 24 24"
         stroke="currentColor"
@@ -22,13 +39,61 @@ export default function Chirp({ chirp }) {
         />
       </svg>
       <div className="flex-1">
-        <div className="flex justify-between items-center">
+        <div className="flex items-center justify-between">
           <div>
             <span className="text-gray-800">{chirp.user.name}</span>
             <small className="ml-2 text-sm text-gray-600">{dayjs(chirp.created_at).fromNow()}</small>
+            {chirp.created_at !== chirp.updated_at && <small className="text-sm text-gray-600"> &middot; edited </small>}
           </div>
+          {chirp.user.id === auth.user.id && (
+            <Dropdown>
+              <Dropdown.Trigger>
+                <button>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="w-4 h-4 text-gray-400"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+                    <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z" />
+                  </svg>
+                </button>
+              </Dropdown.Trigger>
+              <Dropdown.Content>
+                <button
+                  className="block w-full px-4 py-2 text-sm leading-5 text-left text-gray-700 transition duration-150 ease-in-out hover:bg-gray-100 focus:bg-gray-100"
+                  onClick={() => setEditing(true)}
+                >
+                  Edit
+                </button>
+              </Dropdown.Content>
+            </Dropdown>
+          )}
         </div>
-        <p className="mt-4 text-lg text-gray-900">{chirp.message}</p>
+        {editing ? (
+          <form onSubmit={submit}>
+            <textarea
+              value={data.message}
+              onChange={(e) => setData("message", e.target.value)}
+              className="w-full mt-4 text-gray-900 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+            />
+            <InputError
+              message={errors.message}
+              className="mt-2"
+            />
+            <div className="space-x-2">
+              <PrimaryButton className="mt-4">Save</PrimaryButton>
+              <button
+                className="mt-4"
+                onClick={() => setEditing(false) && reset()}
+              >
+                Cancel
+              </button>
+            </div>
+          </form>
+        ) : (
+          <p className="mt-4 text-lg text-gray-900">{chirp.message}</p>
+        )}
       </div>
     </div>
   );
